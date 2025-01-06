bbc7ed248b53e04ec665dc353870ff78cd82eab6
Abhishek6546 <abhishekk11603@gmail.com>
2025-01-06 12:14:12 +0530

    push

--- src/components/Cards2.tsx
+++ src/components/Cards2.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 interface CardData {
     name: string;
